0dabe105957fd1441e0a736af8ea2e527db549f4
Nicholas Sledgianowski <nick@sledgeworx.io>
2021-09-03 15:03:53 -0700

    Readd oauth requirment for ZapierController

--- app/controllers/zapier_controller.rb
+++ app/controllers/zapier_controller.rb
@@ -1,7 +1,7 @@
 #Zapier controller
 
 class ZapierController < ApplicationController
-    # before_action :doorkeeper_authorize!
+    before_action :doorkeeper_authorize!
     def test
         render :json => 200.to_json
     end
