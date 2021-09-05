9047c8107bd7b8ebfe23c4e3bfb969f30bf0fb82
Nicholas Sledgianowski <nick@sledgeworx.io>
2021-09-05 09:06:56 -0700

    Try pulling user from doorkeeper token

--- app/controllers/zapier_controller.rb
+++ app/controllers/zapier_controller.rb
@@ -8,11 +8,16 @@ class ZapierController < ApplicationController
 
     def payment_recieved
         
-        order = current_user.orders.last
+        order = current_resource_owner.orders.last
         unless order.nil?
             render :json => [order].to_json
         else 
             render :json [].to_json
         end
     end
+
+  # Find the user that owns the access token
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
