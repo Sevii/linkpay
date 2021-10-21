286a1e169bb03a195fbf17db1facd59c67d611ed
Nicholas Sledgianowski <nick@sledgeworx.io>
2021-10-21 14:19:51 -0700

    Working on bitcoin payment refactors

--- app/javascript/controllers/meta_controller.js
+++ app/javascript/controllers/meta_controller.js
@@ -2,24 +2,30 @@ import { Controller } from "stimulus"
 import { connect_accounts, pay, onboard, walletCompatible } from '../metamask/index';
 import Rails from '@rails/ujs';
 
+import Web3 from 'web3';
+const web3 = new Web3('ws://localhost:8546');
 
 export default class extends Controller {
-static targets = [ "install", "orderStatus", "payButton", "orderButton", "results", "emailfield", "paymentform" ]
+static targets = [ "install", "orderStatus", "payEthereumButton","payBitcoinButton", "orderButton", "results", "emailfield", "paymentform", "bitcoinQRCode" ]
 static values = { metamaskconnected: Boolean, address: String, inovice: String, productprice: String}
 
-  pay_button() {
-    console.dir(this.element);
-    this.payButtonTarget.disabled = true;
+
+  pay_bitcoin_button() {
+    this.payBitcoinButtonTarget.disabled = true;
+    console.log("Bitcoin payment");
+  }
+
+  pay_ethereum_button() {
+    this.payEthereumButtonTarget.disabled = true;
     console.log("inoviceId: " + this.inoviceValue);
 
     let customerEmail = this.emailfieldTarget.value;
     if(customerEmail == null || customerEmail === "" || customerEmail < 5) {
       this.orderStatusTarget.textContent = "Email not long enough";
-      this.payButtonTarget.disabled = false;
+      this.payEthereumButtonTarget.disabled = false;
       console.log("email: " + customerEmail);
       return false;
     }
-
 
     return fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd")
     .then(response => response.json())
@@ -38,6 +44,7 @@ static values = { metamaskconnected: Boolean, address: String, inovice: String, 
           currency_to_usd: orderDetails.currency_to_usd,
           currency_amount: orderDetails.currency_amount,
           customer_email: customerEmail,
+          currency: "ethereum",
           account: orderDetails.customer_account
         }
         
@@ -65,14 +72,14 @@ static values = { metamaskconnected: Boolean, address: String, inovice: String, 
       if (err.code === 4001) {
         // EIP-1193 userRejectedRequest error
         // If this happens, the user rejected the connection request.
-        this.payButtonTarget.disabled = false;
+        this.payEthereumButtonTarget.disabled = false;
         this.orderStatusTarget.textContent="Payment Rejected";
       } else if(err.code === -32602) {
-        this.payButtonTarget.disabled = false;
+        this.payEthereumButtonTarget.disabled = false;
         this.orderStatusTarget.textContent="Please connect with Metamask Wallet.";
       } else {
         console.error(err);
-        this.payButtonTarget.disabled = false;
+        this.payEthereumButtonTarget.disabled = false;
         this.orderStatusTarget.textContent="Payment failed error code: " + err.code;
       }
     });
@@ -97,13 +104,13 @@ static values = { metamaskconnected: Boolean, address: String, inovice: String, 
         walletCompatible().then(compatible => {
           if(compatible) {
             this.metamaskconnectedValue = true;
-            this.payButtonTarget.disabled = false;
+            this.payEthereumButtonTarget.disabled = false;
             console.log("compatible: " + compatible);
           }
           else {
               this.installTarget.hidden = false;
               this.paymentformTarget.hidden = true;
-              this.payButtonTarget.disabled = true;
+              this.payEthereumButtonTarget.disabled = true;
               console.log("compatible: " + compatible);
           }
         });
